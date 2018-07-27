60c2dd5d57c1d2dd6e0d83e5a3bef1f20fbe7b5f
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-07-27 13:11:04 +0200

    Added Brain.cs

---
+++ Assets/_Project/Platform/Brain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Replay
+{
+    public List<double> States { get; set; }
+    public double Reward { get; set; }
+
+    public Replay (double zRotation, double ballPositionX, double ballVelocityX, double reward)
+    {
+        States = new List<double>();
+        States.Add(zRotation);
+        States.Add(ballPositionX);
+        States.Add(ballVelocityX);
+        Reward = reward;
+    }
+}
+
+public class Brain : MonoBehaviour 
+{
+    
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
